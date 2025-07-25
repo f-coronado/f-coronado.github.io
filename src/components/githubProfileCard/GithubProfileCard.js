017d7184cac6ad5e71f02cb4c93015bf96c90c97
fabrizzio <fabrizzio37@gmail.com>
2025-07-25 12:47:45 -0400

    added more projects

--- src/components/githubProfileCard/GithubProfileCard.js
+++ src/components/githubProfileCard/GithubProfileCard.js
@@ -46,7 +46,7 @@ export default function GithubProfileCard({prof}) {
                 Open for opportunities: {prof.hireable}
               </span>
             </div> */}
-            <SocialMedia />
+            {/* <SocialMedia /> */}
           </div>
           <div className="image-content-profile">
             <img
